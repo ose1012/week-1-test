c65e1cc1ad811536df9eba62ff97c8847af1ff6a
ose1012 <nnmmaz95@gmail.com>
2022-10-06 11:48:40 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTodos((crrArr) => [todo, ...crrArr]);
+  };
+  console.log(todo);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit} className="add-form">
+        <input
+          type="text"
+          name="title"
+          placeholder="자바스크립트를 배워봅시다"
+          onChange={onChange}
+        ></input>
+        <button type="submit">추가하기</button>
+      </form>
+      <h1 className="title">Todo List</h1>
+      {todos.map((item) => {
+        console.log(todos);
+        return (
+          <div className="list" key={item.id}>
+            <p className="item">{item}</p>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
